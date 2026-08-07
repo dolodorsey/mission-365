005fa06b24f88249bf9eedcf4187f0bc71c34239
dolodorsey <dolodorsey@gmail.com>
2026-08-07 04:38:25 -0400

    enable Mission 365 server runtime

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  trailingSlash: true,
+  trailingSlash: false,
   turbopack: { root: __dirname },
 };
 module.exports = nextConfig;
