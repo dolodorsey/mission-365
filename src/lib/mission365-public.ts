252deb3784df7ce1902d77f88f106ea8739cfb9e
dolodorsey <dolodorsey@gmail.com>
2026-08-07 20:50:47 -0400

    wire Mission 365 live admin endpoints

--- src/lib/mission365-public.ts
+++ src/lib/mission365-public.ts
@@ -1,3 +1,5 @@
 export const MISSION365_SUPABASE_URL = 'https://rwpcqeiukrektpjqkpdx.supabase.co'
 export const MISSION365_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_02rYD0XFYvb2AQ2ObQ1hbg_uv5jsd6r'
 export const MISSION365_CHECKOUT_URL = `${MISSION365_SUPABASE_URL}/functions/v1/mission365-checkout`
+export const MISSION365_LAUNCH_STATUS_URL = `${MISSION365_SUPABASE_URL}/functions/v1/mission365-launch-status`
+export const MISSION365_ADMIN_QUEUE_URL = `${MISSION365_SUPABASE_URL}/functions/v1/mission365-admin-queue`
