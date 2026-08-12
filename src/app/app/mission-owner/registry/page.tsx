05a81f93ba6c1f2d6a0d16f3d14ffb19f5946244
dolodorsey <dolodorsey@gmail.com>
2026-08-12 03:23:14 -0400

    Add Mission 365 registry route

---
+++ src/app/app/mission-owner/registry/page.tsx
@@ -0,0 +1,4 @@
+import MissionRegistryManager from './MissionRegistryManager'
+
+export const metadata={title:'Mission Registry | Mission 365'}
+export default function MissionRegistryPage(){return <MissionRegistryManager/>}
