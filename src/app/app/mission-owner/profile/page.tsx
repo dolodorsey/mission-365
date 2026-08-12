1bf2d5d4bd735fe4a4c4c308a9df31ead72083c3
dolodorsey <dolodorsey@gmail.com>
2026-08-12 04:06:58 -0400

    Add Mission Owner profile manager route

---
+++ src/app/app/mission-owner/profile/page.tsx
@@ -0,0 +1,3 @@
+import MissionProfileManager from './MissionProfileManager'
+export const metadata={title:'Mission Profiles | Mission 365'}
+export default function MissionProfilePage(){return <MissionProfileManager/>}
